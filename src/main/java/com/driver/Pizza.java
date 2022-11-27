63d2b7653c8bb413f85aac25862d710ecb4c7be8
Shibu Gope <shibugoper1@gmail.com>
2022-11-27 20:52:17 +0530

    assignment

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,6 +9,14 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+        price=300;	
+        bill="Base Price Of The Pizza: 300\n";
+        }else {
+        	price=400;
+        	 bill="Base Price Of The Pizza: 400\n";
+        }
+       
     }
 
     public int getPrice(){
@@ -17,18 +25,44 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+    	if(bill.contains("Cheese")) {
+    		return;
+    	}
+    	else{
+    		price+=80;
+    	bill+="Extra Cheese Price = 80\n";
+    }
     }
 
     public void addExtraToppings(){
+    	if(bill.contains("Toppings")) {
+    		return;
+    	}
         // your code goes here
+    	else{
+    		if(isVeg){
+    		 price+=70;
+    		 bill+="Extra Toppings For Veg Pizza = 70\n";
+    		}
+    	 else {
+    		 price+=120;
+    		 bill+="Extra Toppings For Non-veg Pizza = 120\n";
+    	 }
+    }
     }
 
     public void addTakeaway(){
         // your code goes here
+    	if(bill.contains("bag"))return;
+    	else{
+    		price+=20;
+    	bill+="Paper bag Price = 20\n";
+    	}
     }
 
     public String getBill(){
         // your code goes here
+    	bill+="Total Price: "+price;
         return this.bill;
     }
 }
